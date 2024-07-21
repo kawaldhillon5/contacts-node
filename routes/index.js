f0236a825c0d12c8cbf1208d5355130f424d2b02
kawaldhillon05 <kawaldhillon5@gmail.com>
2024-07-21 19:57:05 -0400

    added some features

--- routes/index.js
+++ routes/index.js
@@ -18,18 +18,23 @@ router.get('/:contactId', async function(req, res, next) {
   res.render("contact", {contacts: await loader(), contact: con });
 });
 
-router.post('/addNew', async function(req, res, next) {
-  await contactsJs.createContact();
-  res.redirect('/');
-});
-
 router.post('/:contactId/edit', async function(req, res, next) {
   res.render('edit', {contacts: await loader(), contact: await contactsJs.getContact(req.params.contactId)});
 });
 
+router.post('/addNew', async function(req, res, next) {
+  res.render('edit', {contacts: await loader(), contact: await contactsJs.createContact()});
+});
+
+
 router.post('/:contactId/save', async function(req, res, next ){
   await contactsJs.updateContact(req.params.contactId, await req.body);
   res.redirect(`/${req.params.contactId}`);
+});
+
+router.post('/:contactId/destroy', async function(req, res, next) {
+  await contactsJs.deleteContact(req.params.contactId);
+  res.redirect('/');
 });
 
 
